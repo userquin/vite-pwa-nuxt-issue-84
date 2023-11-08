98560bfa26ef32ec2e9cfcadc8f987728f65c0db
userquin <userquin@gmail.com>
2023-11-08 22:42:37 +0100

    chore: include minimal pwa manifest

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,10 +4,25 @@ export default defineNuxtConfig({
   ssr: false,
   modules: ["@vite-pwa/nuxt"], // https://vite-pwa-org.netlify.app/frameworks/nuxt
   pwa: {
+    mode: 'development',
+    registerType: 'autoUpdate',
     /* your pwa options */
+    manifest: {
+      name: "My Nuxt App",
+      short_name: "My Nuxt App",
+      description: "My Nuxt App description",
+      lang: "en",
+    },
+    workbox: {
+      //navigateFallback: '/',
+      globPatterns: ['**/*.{js,css,html,json}'],
+    }
   },
-  generate: {
+  experimental: {
+    payloadExtraction: false
+  }
+  /*generate: {
     routes: ["/"],
     // TODO https://vite-pwa-org.netlify.app/frameworks/nuxt#prompt-for-update-and-offline-ready
-  },
+  },*/
 });
